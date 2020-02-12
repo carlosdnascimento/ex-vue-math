b4f5d864e1d576feffd7e44d5131263ff51f384d
Carlos Matheus Monteiro do Nascimento <carlos.dnascimento@al.infnet.edu.br>
2020-02-12 17:51:04 -0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div> header</div>
+    <HelloWorld msg="Vue.js aula da thais" oi="olá"/>
+    <Quadrado :n="3"/>
+    <Cubo :n="3"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Quadrado from './components/Quadrado.vue'
+import Cubo from './components/Cubo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Quadrado,
+    Cubo
+  },data(){
+    return { n : 4}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
